148ea9b26ee585911ae5620e98231c23d1363256
santiago <santiagorg218@gmail.com>
2024-08-21 09:08:42 -0500

    se agrego el formulario para añadir tareas y se cambio el menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: LayoutComponent,
-        children: [  
+        children: [
+          //{ path: '', component:  },
+          { path: 'Add-Task', component: TaskFormComponent },
+          // Otras rutas...
         ]
       }
 ];
