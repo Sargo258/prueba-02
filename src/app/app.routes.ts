4b3d750de240545f15ca9bb24bb76683e57a1da0
santiago <santiagorg218@gmail.com>
2024-09-17 10:56:45 -0500

    se creo el componente de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,25 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { TaskFormComponent } from './task-form/task-form.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {
         path: '',
+        redirectTo: '/login', 
+        pathMatch: 'full'     
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '',
         component: LayoutComponent,
         children: [
-          { path: '', component:  TodoListComponent},
-          { path: 'Add-Task', component: TaskFormComponent },
-          { path: 'Todo-List', component: TodoListComponent}
-          // Otras rutas...
+            { path: 'Add-Task', component: TaskFormComponent },
+            { path: 'Todo-List', component: TodoListComponent }
+            // Otras rutas...
         ]
-      }
+    }
 ];
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8080/api/login'; // Cambia esta URL si es necesario
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(this.apiUrl, { username, password });
+  }
+  
+}
